f23c40fb385ca318d78adad998c3b6d82870a0ed
coding-to-music <connors.tom@gmail.com>
2020-12-22 04:48:25 -0500

    tweak

--- pageScraper.js
+++ pageScraper.js
@@ -11,8 +11,7 @@ const scraperObject = {
     async scraper(browser){
         let page = await browser.newPage();
         console.log(`Navigating to ${this.url}...`);
-        await page.goto(this.url);
-
+        await page.goto(this.url)
     }
 }
 
