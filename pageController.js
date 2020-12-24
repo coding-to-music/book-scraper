c361f4abd5010cf95abb2b1c474d05e6ab90483f
coding-to-music <connors.tom@gmail.com>
2020-12-24 18:33:07 -0500

    pageController for more categories

--- pageController.js
+++ pageController.js
@@ -13,6 +13,8 @@ async function scrapeAll(browserInstance){
         await pageScraper.scraper(browser);
         // Call the scraper for different set of books to be scraped
         scrapedData['Travel'] = await pageScraper.scraper(browser, 'Travel');
+        scrapedData['HistoricalFiction'] = await pageScraper.scraper(browser, 'Historical Fiction');
+        scrapedData['Mystery'] = await pageScraper.scraper(browser, 'Mystery');
         await browser.close();
         console.log(scrapedData)
     }
